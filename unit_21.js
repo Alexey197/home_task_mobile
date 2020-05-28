8ebdd5ed4a7f37594ef98515c0713bdd1ff30c73
Alexey197 <margadonnn3@gmail.com>
2020-05-28 11:08:38 +0300

    unit-22

--- unit_21.js
+++ unit_21.js
@@ -1,200 +1,95 @@
 
 // Task 1 ============================================
-/* Создайте блок div-1. Добавьте на него событие touchstart. Выведите в out-1 слово  touch если событие сработает. */
-
+/* Создайте блок div-1. Добавьте на него событие onclick. При срабатывании события  выводите в out-1 количество срабатываний события. В комментариях опишите результат. */
+let count = 0;
 function t1() {
-    document.querySelector('.out-1').textContent = 'touch';
+    count++;
+    document.querySelector('.out-1').textContent = count;
 }
+document.querySelector('.div-1').onclick = t1;
 
-document.querySelector('.div-1').addEventListener('touchstart', t1);
 // ваше событие здесь!!!
 
 // Task 2 ============================================
-/* Создайте блок div-2. Добавьте на него событие touchstart. Выведите в out-2 число срабатываний события. */
+/* Создайте блок div-2. Добавьте на него событие ondblclick. При срабатывании события  выводите в out-2 количество срабатываний события. В комментариях опишите результат. */
 
-function t2(event) {
-    document.querySelector('.out-2').textContent = event.touches.length;
+function t2() {
+
 }
-document.querySelector('.div-2').addEventListener('touchstart', t2);
 
 // ваше событие здесь!!!
 
 
 // Task 3 ============================================
-/*  Создайте блок div-3_1 и div-3_2. Добавьте на них событие touchstart. Выведите в out-3 номер блока 1 или 2 на котором сработало событие. */
+/*  Создайте блок div-3. Добавьте на него событие onmousemove. При срабатывании события выводите в out-3 количество срабатываний события. В комментариях опишите результат. */
 
-function t3(event) {
-    const out3 = document.querySelector('.out-3');
-    if (event.target.textContent === '3-1') out3.textContent = '1';
-    if (event.target.textContent === '3-2') out3.textContent = '2';
+function t3() {
+
 }
 
-document.querySelector('.div-3_1').addEventListener('touchstart', t3);
-document.querySelector('.div-3_2').addEventListener('touchstart', t3);
 // ваше событие здесь!!!
 
 
 // Task 4 ============================================
-/*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
-
-const div4 = document.querySelector('.div-4');
-const btn4 = document.createElement('button');
-btn4.classList.add('button-primary');
-btn4.classList.add('b-4');
-btn4.textContent = 'Task-4';
-div4.after(btn4);
+/*  Создайте блок div-4. Добавьте на него событие oncontextmenu. При срабатывании события выводите в out-4 количество срабатываний события. В комментариях опишите результат. */
 
 function t4() {
-    document.querySelector('.out-4').textContent = 'touch';
+
 }
 
-btn4.onclick = () => {
-    div4.addEventListener('touchstart', t4);
-};
 // ваше событие здесь!!!
 
 // Task 5 ============================================
-/*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
+/*   Создайте блок div-5. Добавьте на него событие onmousedown. При срабатывании события выводите в out-5 количество срабатываний события. В комментариях опишите результат. */
 
 function t5() {
-    div4.removeEventListener('touchstart', t4);
+
 }
 
-document.querySelector('.b-5').onclick = t5;
 // ваше событие здесь!!!
 
 // Task 6 ============================================
-/*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
+/*  Создайте блок div-6. Добавьте на него событие onmouseenter. При срабатывании события выводите в out-6 количество срабатываний события. В комментариях опишите результат. */
 
 function t6() {
-    document.querySelector('.out-6').textContent = 'touchend';
+
 }
 
-div4.ontouchend = t6;
 // ваше событие здесь!!!
 
 
 // Task 7 ============================================
-/*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
+/*   Создайте блок div-7. Добавьте на него событие onmouseleave. При срабатывании события выводите в out-7 количество срабатываний события. В комментариях опишите результат.*/
 
 function t7() {
-    this.style.backgroundColor = 'red';
+
 }
 
-document.querySelector('.div-7').addEventListener('touchstart', t7);
 // ваше событие здесь!!!
 
 // Task 8 ============================================
-/*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
-const a8=['red', 'green', 'blue', 'orange', 'pink', 'yellow']
+/*   Создайте блок div-8. Добавьте на него событие onmouseout. При срабатывании события выводите в out-8 количество срабатываний события. В комментариях опишите результат.*/
 
 function t8() {
-    this.style.backgroundColor = a8[Math.floor(Math.random() * a8.length)];
+
 }
 
-document.querySelector('.div-8').addEventListener('touchstart', t8);
 // ваше событие здесь!!!
 
 
 // Task 9 ============================================
-/* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
+/* Создайте блок div-9. Добавьте на него событие onmouseover. При срабатывании события выводите в out-9 количество срабатываний события. В комментариях опишите результат. */
 
-function t9(event) {
-    document.querySelector('.out-9').textContent = event.touches.length;
+function t9() {
+
 }
 
-document.querySelector('.div-9').addEventListener('touchstart', t9);
 // ваше событие здесь!!!
 
 
 // Task 10 ============================================
-/*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
-
-const div10 = document.querySelector('.div-10');
-let w10 = div10.offsetWidth;
+/*  Создайте блок div-10. Добавьте на него событие onmouseup. При срабатывании события выводите в out-10 количество срабатываний события. В комментариях опишите результат.*/
 
 function t10() {
-    w10++;
-    div10.style.width = w10 + 'px';
+
 }
-
-div10.addEventListener('touchmove', t10);
-// ваше событие здесь!!!
-
-// Task 11 ============================================
-/*  Дан блок div-11. Добавьте на него событие touch. При срабатывании выводите радиус события radiusX, radiusY. */
-
-function t11(event) {
-    let out = '';
-    out = event.touches[0].radiusX + ' ' + event.touches[0].radiusY;
-    document.querySelector('.out-11').textContent = out;
-}
-
-document.querySelector('.div-11').addEventListener('touchstart', t11);
-// ваше событие здесь!!!
-
-// Task 12 ============================================
-/*  Мини проект. Ознакомьтесь с версткой в задании 12.
-<p>Изучите html код внутри div-12-wrapper.</p>
-<p>1. Добавьте на кнопку nex событие click, touch так, чтобы при событии запускалась функция nextFunction и активным становилось изображение следующее за выделенным классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
-<p>2. Добавьте на кнопку prev событие click, touch так, чтобы при событии запускалась функция prevFunction и активным становилось изображение до выделенного классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
-<p>3. Учтите краевые эффекты - когда мы доходим до конца или начала, то нажатие кнопки должно приводить к перемещению рамки в начало или конец изображений.</p>
-<p>4. Добавьте кнопку reset (функция resetFunction), нажатие которой сбрасывает активное изображение на нулевое. </p>
-<p>5. Добавьте во все действия следующее - в изображении img-12-max заменяется src на активную. Т.е. произошло событие - заменилась главная картинка.</p>
-*/
-
-const images = document.querySelectorAll('.img-12-min');
-let count = 0; // переменная, которая указывает на номер активного изображения в images
-
-const next = document.querySelectorAll('.next');
-next[0].onclick = nextFunction;
-next[0].ontouchstart = nextFunction;
-
-const prev = document.querySelectorAll('.prev');
-prev[0].onclick = prevFunction;
-prev[0].ontouchstart = prevFunction;
-
-const reset = document.createElement('button');
-reset.classList.add('button-primary');
-reset.textContent = 'RESET';
-document.querySelector('.div-12-wrapper').append(reset);
-
-function nextFunction() {
-    images[count].classList.remove('active-img');
-    count++;
-    if (count === images.length) {
-        count = 0;
-        images[count].classList.add('active-img');
-    }
-    images[count].classList.add('active-img');
-    document.querySelector('.div-12-max img').src = `img/${count + 1}.png`;
-}
-
-function prevFunction() {
-    images[count].classList.remove('active-img');
-    if (count === 0) {
-        count = images.length;
-        images[count - 1].classList.add('active-img');
-    }
-    count--;
-    images[count].classList.add('active-img');
-    document.querySelector('.div-12-max img').src = `img/${count + 1}.png`;
-}
-
-function resetFunction() {
-    images.forEach((element) => element.classList.remove('active-img'));
-    images[0].classList.add('active-img');
-    count = 0;
-    document.querySelector('.div-12-max img').src = "img/1.png";
-}
-
-next[0].ontouchstart = (event) => {
-    event.preventDefault();
-    nextFunction();
-}
-prev[0].ontouchstart = (event) => {
-    event.preventDefault();
-    prevFunction();
-}
-reset.onclick = resetFunction;
-// ваше событие здесь!!!
