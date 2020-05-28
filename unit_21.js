c17cfe4d3f01f79ef6bc2d9001ab025453494248
Alexey197 <margadonnn3@gmail.com>
2020-05-28 19:28:35 +0300

    unit-22 complete

--- unit_21.js
+++ unit_21.js
@@ -40,27 +40,33 @@ document.querySelector('.div-3').onmousemove = t3;
 /*  Создайте блок div-4. Добавьте на него событие oncontextmenu. При срабатывании события выводите в out-4 количество срабатываний события. В комментариях опишите результат. */
 
 function t4() {
-
+    count++;
+    document.querySelector('.out-4').textContent = count;
 }
 
+document.querySelector('.div-4').oncontextmenu = t4;
 // ваше событие здесь!!!
 
 // Task 5 ============================================
 /*   Создайте блок div-5. Добавьте на него событие onmousedown. При срабатывании события выводите в out-5 количество срабатываний события. В комментариях опишите результат. */
 
 function t5() {
-
+    count++;
+    document.querySelector('.out-5').textContent = count;
 }
 
+document.querySelector('.div-5').onmousedown = t5;
 // ваше событие здесь!!!
 
 // Task 6 ============================================
 /*  Создайте блок div-6. Добавьте на него событие onmouseenter. При срабатывании события выводите в out-6 количество срабатываний события. В комментариях опишите результат. */
 
 function t6() {
-
+    count++;
+    document.querySelector('.out-6').textContent = count;
 }
 
+document.querySelector('.div-6').onmouseenter = t6;
 // ваше событие здесь!!!
 
 
@@ -68,18 +74,22 @@ function t6() {
 /*   Создайте блок div-7. Добавьте на него событие onmouseleave. При срабатывании события выводите в out-7 количество срабатываний события. В комментариях опишите результат.*/
 
 function t7() {
-
+    count++;
+    document.querySelector('.out-7').textContent = count;
 }
 
+document.querySelector('.div-7').onmouseleave = t7;
 // ваше событие здесь!!!
 
 // Task 8 ============================================
 /*   Создайте блок div-8. Добавьте на него событие onmouseout. При срабатывании события выводите в out-8 количество срабатываний события. В комментариях опишите результат.*/
 
 function t8() {
-
+    count++;
+    document.querySelector('.out-8').textContent = count;
 }
 
+document.querySelector('.div-8').onmouseout = t8;
 // ваше событие здесь!!!
 
 
@@ -87,9 +97,11 @@ function t8() {
 /* Создайте блок div-9. Добавьте на него событие onmouseover. При срабатывании события выводите в out-9 количество срабатываний события. В комментариях опишите результат. */
 
 function t9() {
-
+    count++;
+    document.querySelector('.out-9').textContent = count;
 }
 
+document.querySelector('.div-9').onmouseover = t9;
 // ваше событие здесь!!!
 
 
@@ -97,5 +109,8 @@ function t9() {
 /*  Создайте блок div-10. Добавьте на него событие onmouseup. При срабатывании события выводите в out-10 количество срабатываний события. В комментариях опишите результат.*/
 
 function t10() {
-
+    count++;
+    document.querySelector('.out-10').textContent = count;
 }
+
+document.querySelector('.div-10').onmouseup = t10;
